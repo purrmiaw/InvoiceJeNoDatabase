43cd28884c516de9ab7d3f0013502d813c4391c8
Purr Miaw <purr@miaw.xyz>
2017-07-18 10:52:00 +0800

    Done Storyboard flow

--- InvoiceJe/InvoiceJe.iOS/ViewController.cs
+++ InvoiceJe/InvoiceJe.iOS/ViewController.cs
@@ -1,33 +1,33 @@
-﻿using System;
+﻿//using System;
 
-using UIKit;
+//using UIKit;
 
-namespace InvoiceJe.iOS
-{
-	public partial class ViewController : UIViewController
-	{
-		int count = 1;
+//namespace InvoiceJe.iOS
+//{
+//	public partial class ViewController : UIViewController
+//	{
+//		int count = 1;
 
-		public ViewController (IntPtr handle) : base (handle)
-		{
-		}
+//		public ViewController (IntPtr handle) : base (handle)
+//		{
+//		}
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			// Perform any additional setup after loading the view, typically from a nib.
-			Button.AccessibilityIdentifier = "myButton";
-			Button.TouchUpInside += delegate {
-				var title = string.Format ("{0} clicks!", count++);
-				Button.SetTitle (title, UIControlState.Normal);
-			};
-		}
+//		public override void ViewDidLoad ()
+//		{
+//			base.ViewDidLoad ();
+//			// Perform any additional setup after loading the view, typically from a nib.
+//			Button.AccessibilityIdentifier = "myButton";
+//			Button.TouchUpInside += delegate {
+//				var title = string.Format ("{0} clicks!", count++);
+//				Button.SetTitle (title, UIControlState.Normal);
+//			};
+//		}
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			base.DidReceiveMemoryWarning ();
-			// Release any cached data, images, etc that aren't in use.
-		}
-	}
-}
+//		public override void DidReceiveMemoryWarning ()
+//		{
+//			base.DidReceiveMemoryWarning ();
+//			// Release any cached data, images, etc that aren't in use.
+//		}
+//	}
+//}
 
