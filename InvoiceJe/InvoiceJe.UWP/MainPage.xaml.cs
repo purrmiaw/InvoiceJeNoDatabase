d3de67d385e9e951b60547335d498526c29945e5
Purr Miaw <purr@miaw.xyz>
2017-07-21 11:29:38 +0800

    UWP

--- InvoiceJe/InvoiceJe.UWP/MainPage.xaml.cs
+++ InvoiceJe/InvoiceJe.UWP/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace InvoiceJe.UWP
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
